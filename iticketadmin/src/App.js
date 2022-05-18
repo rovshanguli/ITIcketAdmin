7d307fdb3c12e32d0da44cf1384c9fbd1b944552
rovshanguli <rovshanshg@code.edu.az>
2022-05-18 20:18:06 +0400

    SideBar

--- iticketadmin/src/App.js
+++ iticketadmin/src/App.js
@@ -1,9 +1,23 @@
-import './assets/css/style.css';
-import SideBar from './components/layout/SideBar'; 
+import './assets/css/style.css'
+import SideBar from './components/layout/SideBar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   return (
-    <div classNameName="">
-        <SideBar/>
+    <div >
+
+      <Router>
+        <div>
+
+
+
+          <Routes>
+
+            <Route exact path="/" element={<SideBar />} />
+          </Routes>
+
+        </div>
+      </Router>
+
     </div>
   );
 }
